ce137b1b504e409740927834deb464d801d9940d
Jason Chow <79327473+JasonChow320@users.noreply.github.com>
2022-12-08 00:31:32 -0500

    Update config.js

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -8,8 +8,8 @@ const config = {
       name: 'DEV'
     },
     spotify : {
-        SPOTIFY_CLIENTID : '5424d2905c1249b7af5d4d24a3dee826',
-        SPOTIFY_SECRETID : 'dd7b9f6d8b724898bdd122e4a0e8f26b',
+        SPOTIFY_CLIENTID : 'NEED TO GET YOUR OWN API KEYS',
+        SPOTIFY_SECRETID : 'NEED TO GET YOUR OWN API KEYS',
 
         callback : 'http://localhost:3001/spotifyapi/callback/'
     },
